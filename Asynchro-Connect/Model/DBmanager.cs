3116a599c803960d23c091dc638da9524106cd90
Ohi <ohi-ii@att.net>
2021-02-13 19:41:47 -0700

    base firestore funcionality added

---
+++ Asynchro-Connect/Model/DBmanager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+namespace Asynchro_Connect.Model
+{
+    public class DBmanager
+    {
+        private FirestoreDb db;
+
+        public DBmanager() {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "..\\..\\.env\\Asynchro-Connect-cc3f69022ee5.json"); 
+            this.db = FirestoreDb.Create("asynchro-connect-304800");
+        }
+
+        public async Task test() {
+            DocumentReference docRef = db.Collection("users").Document("alovelace");
+            Dictionary<string, object> user = new Dictionary<string, object>
+            {
+                { "First", "Ada" },
+                { "Last", "Lovelace" },
+                { "Born", 1815 }
+            };
+            await docRef.SetAsync(user);
+        }
+    }
+}
--- Asynchro-Connect/Program.cs
+++ Asynchro-Connect/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Asynchro_Connect.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Asynchro_Connect
 {
@@ -12,11 +14,16 @@ namespace Asynchro_Connect
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            //Console.WriteLine("Created Cloud Firestore client with project ID: {0}", "Asynchro-Connect");
+            DBmanager dbm = new DBmanager();
+            dbm.test();
             Application.Run(new View.LoginView());
+            
         }
     }
 }
