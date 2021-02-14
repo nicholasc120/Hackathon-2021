67720e6fb123a458931395416f54794f8306ded1
NicholasC120 <>
2021-02-13 20:47:20 -0700

    adds study group homepage

---
+++ Asynchro-Connect/View/StudyGroupHomepage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Asynchro_Connect.Model;
+
+namespace Asynchro_Connect.View
+{
+    public partial class StudyGroupHomepage : Form
+    {
+        User theUser;
+        StudyGroup theStudyGroup;
+        public StudyGroupHomepage(User theUser, StudyGroup theStudyGroup)
+        {
+            this.theUser = theUser;
+            this.theStudyGroup = theStudyGroup;
+
+            if(theStudyGroup.Admin != theUser)
+            {
+                kickUserButton.Visible = false;
+            }
+            InitializeComponent();
+        }
+
+        private void MemberListBox_Leave(object sender, EventArgs e)
+        {
+            kickUserButton.Enabled = false;
+        }
+
+        private void MemberListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if user is admin
+            kickUserButton.Enabled = true;
+        }
+    }
+}
